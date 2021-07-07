009b76a789697f52a4b8e16d3b78faf365d6306d
Hutyr <32871477+SteveHsiao@users.noreply.github.com>
2021-07-07 12:38:52 +0800

    Add files via upload

---
+++ lesson3hw.py
@@ -0,0 +1,40 @@
+import random
+
+playagain = True
+
+while playagain:
+    num = random.randint(1,20)
+    count = 0
+    print(num)
+
+    while count < 5:
+        number = int(input("please enter 1~20 :"))
+        count = count + 1
+
+        if num == number:
+            print("You are right!")
+            print("You have guessed %d times" % count )
+            break
+        elif number > num:
+            print("Your number is bigger than the answer!")
+        
+        elif number < num:
+            print("Your number is smaller than the answer!")
+        
+
+    print("You failed the challange")
+    answer = input ("Do you want to play again? (yes/no) :")
+    
+    if answer == "yes":
+        playagain = True
+    elif answer == "no":
+        playagain = False
+    
+    
+
+
+
+
+
+    
+
